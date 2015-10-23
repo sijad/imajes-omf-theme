d15fce513e0a88573432860e1f13a610580a92c3
Sajjad Hasehmian <wolaws@gmail.com>
2015-10-23 12:54:56 +0330

    Add base and time

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,3 +1,18 @@
 function fish_right_prompt
-    set -l last_status $status
+    set_color $fish_color_autosuggestion ^/dev/null; or set_color 555
+
+    if [ -n "$SSH_CONNECTION" ]
+      printf '%s | ' (hostname | head -c 10)
+    end
+    if [ "$HOME" = (pwd) ]
+      printf "~"
+    else
+      printf (basename (pwd))
+    end
+
+    set_color normal
+
+    printf " "
+
+    date "+%H:%M:%S"
 end
